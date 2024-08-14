ab9b9fe887d41545ffe6b3bbfb02a96a2c2e951d
Nightsay2002 <wingwf57@gmail.com>
2024-08-14 14:52:46 +0800

    Initial commit

--- app.py
+++ app.py
@@ -18,4 +18,5 @@ def log_time():
     return {"message": "Time logged successfully", "time": current_time}
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8000,debug=True)
+
